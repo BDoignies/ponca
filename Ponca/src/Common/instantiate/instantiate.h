91f19d0f27d2bbcd7bcf8e0d43ce1e9d218e33c9
Bastien DOIGNIES <bastien.doignies@liris.cnrs.fr>
2026-05-19 10:16:18 +0200

    [common] Change declspec location to template declaration

--- Ponca/src/Common/instantiate/instantiate.h
+++ Ponca/src/Common/instantiate/instantiate.h
@@ -7,8 +7,18 @@
 
 #ifdef _PONCA_SHARED_LIBRARY
 #    define _INSTANTIATE_PONCA_EXTERN
+#    ifdef _MSC_VER
+#        define _PONCA_API __declspec(dllexport)
+#    else
+#        define _PONCA_API 
+#    endif
 #else
 #    define _INSTANTIATE_PONCA_EXTERN extern
+#    ifdef _MSC_VER
+#        define _PONCA_API __declspec(dllimport)
+#    else
+#        define _PONCA_API 
+#    endif
 #endif
 
 #include "../../../Ponca"
@@ -17,8 +27,8 @@ namespace Ponca
 {
 // Note: Variadic macros here because ',' in template definition are parsed as different arguments
 // we could use a trick by wrapping the class in parenthesis, but for now this works and is simpler.
-#define _PONCA_BASKET_X(name, desc, ...) _INSTANTIATE_PONCA_EXTERN template class _PONCA_EXPORT __VA_ARGS__;
-#define _PONCA_BASKET_DIFF_X(name, desc, ...) _INSTANTIATE_PONCA_EXTERN template class _PONCA_EXPORT __VA_ARGS__;
+#define _PONCA_BASKET_X(name, desc, ...) _INSTANTIATE_PONCA_EXTERN template class _PONCA_API __VA_ARGS__;
+#define _PONCA_BASKET_DIFF_X(name, desc, ...) _INSTANTIATE_PONCA_EXTERN template class _PONCA_API __VA_ARGS__;
 #include "instantiate_list/entrypoint.h"
 #undef _PONCA_BASKET_X
 #undef _PONCA_BASKET_DIFF_X
